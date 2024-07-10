c85f9f04b01048f7a792f62d6adf2d7cc14891af
lttsz123 <libaifst1@gmail.com>
2024-07-10 16:05:20 +0800

    Update check_test.sh

--- check_test.sh
+++ check_test.sh
@@ -24,30 +24,30 @@ network_height=$(curl -s -X 'GET' 'https://allora-rpc.edgenet.allora.network/abc
 echo '${network_height}'
 echo "检查 Worker 节点: "
 # 使用curl命令  
-curl --location "http://localhost:$port/api/v1/functions/execute" --header 'Content-Type: application/json' --data '{  
-    "function_id": "bafybeigpiwl3o73zvvl6dxdqu7zqcub5mhg65jiky2xqb4rdhfmikswzqm",  
-    "method": "allora-inference-function.wasm",  
-    "parameters": null,  
-    "topic": "1",  
-    "config": {  
-        "env_vars": [  
+curl --location "http://localhost:$port/api/v1/functions/execute" --header 'Content-Type: application/json' --data "{  
+    \"function_id\": \"bafybeigpiwl3o73zvvl6dxdqu7zqcub5mhg65jiky2xqb4rdhfmikswzqm\",  
+    \"method\": \"allora-inference-function.wasm\",  
+    \"parameters\": null,  
+    \"topic\": \"1\",  
+    \"config\": {  
+        \"env_vars\": [  
             {  
-                "name": "BLS_REQUEST_PATH",  
-                "value": "/api"  
+                \"name\": \"BLS_REQUEST_PATH\",  
+                \"value\": \"/api\"  
             },  
             {  
-                "name": "ALLORA_ARG_PARAMS",  
-                "value": "ETH"  
-            },
-             {
-                "name": "ALLORA_BLOCK_HEIGHT_CURRENT",
-                "value": "${network_height}"
-            }
+                \"name\": \"ALLORA_ARG_PARAMS\",  
+                \"value\": \"ETH\"  
+            },  
+            {  
+                \"name\": \"ALLORA_BLOCK_HEIGHT_CURRENT\",  
+                \"value\": \"${network_height}\"  
+            }  
         ],  
-        "number_of_nodes": -1,  
-        "timeout": 10  
+        \"number_of_nodes\": -1,  
+        \"timeout\": 10  
     }  
-}'
+}"
     echo "检查 Updater 节点: curl http://localhost:$((8000 + port_offset))/update"
     curl http://localhost:$((8000 + port_offset))/update
     echo ""
