6d1a3d8fd5beb25a8f08690a28d6f96ce6bfd3ae
lttsz123 <libaifst1@gmail.com>
2024-08-15 19:15:20 +0800

    Update allora_node.sh

--- allora_node.sh
+++ allora_node.sh
@@ -4,6 +4,7 @@ cd allora-chain
 mv docker-compose.yaml bak.docker-compose.yaml.bak
 wget -O docker-compose.yaml https://raw.githubusercontent.com/lttsz123/allora_test/main/docker-compose.yaml
 cd scripts
+rm -rf l1_node.sh
 wget -O l1_node.sh https://raw.githubusercontent.com/lttsz123/allora_test/main/l1_node.sh
 chmod +x l1_node.sh
 cd ..
