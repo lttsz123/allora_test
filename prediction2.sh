93d4e810675b75cec5554932051047072bc4ecec
lttsz123 <libaifst1@gmail.com>
2024-08-15 18:44:22 +0800

    Update prediction2.sh

--- prediction2.sh
+++ prediction2.sh
@@ -82,7 +82,7 @@ setup_instance() {
     "alloraHomeDir": "",
     "gas": "1000000",
     "gasAdjustment": 1.0,
-    "nodeRpc": "https://allora-rpc.testnet-1.testnet.allora.network/",
+    "nodeRpc": "https://158.220.116.212:26657/",
     "maxRetries": 1,
     "delay": 1,
     "submitTx": false
@@ -181,7 +181,7 @@ main() {
  #read -p "请输入要运行的实例开始数字: " num_start
   #read -p "请输入要运行的实例结束数字: " num_instances
   num_start=1
-  num_instances=80
+  num_instances=11
 
 #   for ((i = 1; i <= num_instances; i++)); do
 #     read -p "请输入实例 #${i} 的钱包助记词: " wallet_seed
