a0afbd3fe3aabf5134bedf33c5ba7290d6a4db36
lttsz123 <libaifst1@gmail.com>
2024-08-15 19:13:06 +0800

    Add files via upload

---
+++ allora_node.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git clone https://github.com/allora-network/allora-chain.git
+cd allora-chain
+mv docker-compose.yaml bak.docker-compose.yaml.bak
+wget -O docker-compose.yaml https://raw.githubusercontent.com/lttsz123/allora_test/main/docker-compose.yaml
+cd scripts
+wget -O l1_node.sh https://raw.githubusercontent.com/lttsz123/allora_test/main/l1_node.sh
+chmod +x l1_node.sh
+cd ..
+docker-compose pull
+docker-compose up -d
